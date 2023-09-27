79f5e186ce8f1dacff730474ef5233f1ae66b2c0
C1325058 Igor Marinho da Silva <c1325058@interno.bb.com.br>
2023-09-27 15:45:24 -0300

    componenetelist

---
+++ project_teste/frontend/src/ListComponente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function ListaItems(){
+    return (<ul>
+        <li>teste 1 </li>
+        <li>teste 2 </li>
+        <li>teste 3 </li>
+    </ul>)
+}
+
+export default ListaItems;
